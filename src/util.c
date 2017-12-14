8efaa8a1dd880dba99a423c7537c8078320387bb
Gijs Molenaar <gijs@pythonic.nl>
2017-12-14 14:37:09 +0200

    add more helpers

--- src/util.c
+++ src/util.c
@@ -22,3 +22,52 @@ der_put_empty() {
     return retval;
 }
 
+
+
+int der_get_char(dercursor *cursor, char *val )
+{
+    if (cursor->derlen == 0)
+        return -1;
+    *val = *cursor->derptr;
+
+    return 0;
+}
+
+int der_get_uchar(dercursor *cursor, unsigned char *val )
+{
+    if (cursor->derlen == 0)
+        return -1;
+    *val = *cursor->derptr;
+
+    return 0;
+}
+
+
+int der_get_long (dercursor cursor, long int *valp)
+{
+    return der_get_int32 (cursor, (int32_t *)valp);
+};
+
+int der_get_ulong (dercursor cursor,  long unsigned int *valp)
+{
+    return der_get_uint32 (cursor, (u_int32_t *)valp);
+};
+
+dercursor der_put_long (der_buf_long_t *der_buf_long, long int value)
+{
+    return der_put_int32 ((u_int8_t *)der_buf_long,(u_int32_t) value);
+};
+
+dercursor der_put_ulong (der_buf_ulong_t *der_buf_ulong, long int value)
+{
+    return der_put_uint32 ((u_int8_t *)der_buf_ulong, (u_int32_t)value);
+};
+
+dercursor der_put_char (der_buf_char_t *der_buf_char, char value)
+{
+    dercursor retval;
+    retval.derptr = (uint8_t *)der_buf_char;
+    retval.derlen = 1;
+    *retval.derptr = (uint8_t)value;
+    return retval;
+};
--- src/util.h
+++ src/util.h
@@ -6,11 +6,20 @@
 #include "pkcs11/pkcs11unix.h"
 #include "quick-der/api.h"
 
-CK_RV
-der_error_helper(int error);
+CK_RV der_error_helper(int error);
+dercursor der_put_empty();
+int  der_get_char(dercursor *cursor, char *val );
+int der_get_uchar(dercursor *cursor, unsigned char *val );
+int der_get_long (dercursor cursor, long int *valp);
+int der_get_ulong (dercursor cursor,  long unsigned int *valp);
 
-dercursor
-der_put_empty();
+typedef long int der_buf_long_t;
+dercursor der_put_long (der_buf_long_t *der_buf_long, long int value);
 
+typedef unsigned long int der_buf_ulong_t;
+dercursor der_put_ulong (der_buf_ulong_t *der_buf_ulong, long int value);
+
+typedef uint8_t der_buf_char_t [1];
+dercursor der_put_char (der_buf_char_t *der_buf_char, char value);
 
 #endif //KEEHIVE_UTIL_H
