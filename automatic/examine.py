d6790693f96e45be2326c2ec064f5357a0a70044
Gijs Molenaar <gijs@pythonic.nl>
2018-01-26 11:02:27 +0200

    restructure python script

--- automatic/examine.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-with open('dump', 'rb') as f:
-    data = pickle.load(f)
-    for call in data['calls']:
-        for c in call.type_decl.components:
-            if c.type_decl.type_name == "CHOICE":
-                c
--- automatic/generate.py
+++ automatic/generate.py
@@ -1,8 +1,14 @@
-from asn1ate.sema import TypeAssignment, ComponentType
-from jinja2 import Environment, FileSystemLoader, StrictUndefined
+"""
+Python script to parse remote PKCS11 ASN1 file and generate C project that implements RPC calls
+"""
 from typing import Generator, Tuple, Union
-import pickle
 import os
+import logging
+import pickle
+from quick_der.main import realise
+from asn1ate.sema import TypeAssignment, ComponentType, dependency_sort
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
+
 
 templates_folder = 'templates'
 
@@ -15,6 +21,40 @@ functions_skip = (
     "C-UnlockMutex",
     "C-Notify"
 )
+
+asn1_file = "../data/RemotePKCS11.asn1"
+dump_file = 'dump'
+
+
+logger = logging.getLogger(__name__)
+
+
+def parse(pickle_file):
+    files = [asn1_file]
+    incdirs = []
+    logger.info("parsing ASN1")
+    defmods, refmods = realise(incdirs, files)
+
+    returns = []
+    calls = []
+    others = []
+
+    for mod_name, module in defmods.items():
+
+        for sorted_ass in dependency_sort(module.assignments):
+            for ass in sorted_ass:
+                if ass.type_name.endswith("-Return"):
+                    returns.append(ass)
+                elif ass.type_name.endswith("-Call"):
+                    calls.append(ass)
+                else:
+                    others.append(ass)
+
+    data = {'returns': returns, 'calls': calls, 'others': others}
+
+    with open(pickle_file, 'wb') as f:
+        pickle.dump(data, f)
+    return data
 
 
 def under(s: str) -> str:
@@ -146,7 +186,7 @@ def combined_args(call_func: TypeAssignment, return_func: TypeAssignment):
     return array
 
 
-def main():
+def main(pickle_file):
     env = Environment(
         loader=FileSystemLoader(templates_folder),
         undefined=StrictUndefined,
@@ -160,8 +200,13 @@ def main():
     env.globals['combined_args'] = combined_args
     env.globals['extract_args'] = extract_args
 
-    with open('dump', 'rb') as f:
-        data = pickle.load(f)
+    if not os.access(pickle_file, os.R_OK):
+        logger.info("no dump file, parsing asn1")
+        data = parse(pickle_file)
+    else:
+        logger.info("dump file found, not parsing asn1. remove dump file if you want to parse again.")
+        with open(pickle_file, 'rb') as f:
+            data = pickle.load(f)
 
     data['returns'] = [i for i in data['returns'] if i.type_name[:-7] not in functions_skip]
     data['calls'] = [i for i in data['calls'] if i.type_name[:-5] not in functions_skip]
@@ -181,10 +226,11 @@ def main():
             template = env.get_template(file_path)
 
             target = os.path.join('generated/', file_path)
-            print("generating {}".format(target))
+            logger.info("generating {}".format(target))
             with open(target, 'w') as f:
                 f.write(template.render(**data))
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO)
+    main(dump_file)
